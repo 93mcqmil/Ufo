6c4332a2cec718556ab7abf5e89036a614166b66
93mcqmil <mcqueen_1993@hotmail.com>
2024-02-20 16:03:53 +0100

    Made a button component with som styling

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Components/buttons.css";
+
+// import styles from "myContainer";
+
+const App = () => {
+  return (
+    <div className='styles.myContainer'>
+      <h1>🍌 Hello World! 🛸</h1>;<button>Click me</button>
+      <button className='second-btn'>Enter</button>
+    </div>
+  );
+};
+
+export default App;
